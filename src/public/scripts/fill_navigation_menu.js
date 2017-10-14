56bf5f01d9fa8694aef6420d9b3bb247df91475d
Tatsiana <tatsiana_0607@tut.by>
2017-10-14 15:53:08 +0300

    Fixed unauthorized menu

--- src/public/scripts/fill_navigation_menu.js
+++ src/public/scripts/fill_navigation_menu.js
@@ -6,15 +6,21 @@
 (function ($) {
     $.fn.fill_navigation_menu = function (menu_elements) {
         var menu_ul = document.getElementById("navigation_menu");
+
         $(menu_elements.menu).each(function( index )
         {
-            //var text = '<a href="javascript: loadPage(\'/'+menu_elements.menu[index].link+'\');" >' + menu_elements.menu[index].name + '<a/>';
-            var text = '<a href="/'+menu_elements.menu[index].link+'" >' + menu_elements.menu[index].name + '<a/>';
-            var li = document.createElement("li");
-            li.innerHTML = text;
-            // Вырезаем лишний тег <a>
-            li.innerHTML = li.innerHTML.substring(0, li.innerHTML.length - 8);
-            menu_ul.appendChild(li);
+
+            if(menu_elements.menu[index].need_authorize && localStorage.getItem('login') === null)
+            {return false;}
+
+                //var text = '<a href="javascript: loadPage(\'/'+menu_elements.menu[index].link+'\');" >' + menu_elements.menu[index].name + '<a/>';
+                var text = '<a href="/'+menu_elements.menu[index].link+'" >' + menu_elements.menu[index].name + '<a/>';
+                var li = document.createElement("li");
+                li.innerHTML = text;
+                // Вырезаем лишний тег <a>
+                li.innerHTML = li.innerHTML.substring(0, li.innerHTML.length - 8);
+                menu_ul.appendChild(li);
+
         });
     }
 })(jQuery);
